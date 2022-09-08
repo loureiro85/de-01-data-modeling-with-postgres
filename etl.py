75f8664ae991524273a96093944c7161611833af
Gui-laptop <guilhermel@kaspien.com>
2022-09-08 17:12:04 -0300

    feat: Implemented etl.py

--- etl.py
+++ etl.py
@@ -1,43 +1,93 @@
-import os
 import glob
-import psycopg2
+import os
+
 import pandas as pd
-from sql_queries import *
+import psycopg2
+
+from sql_queries import (artist_table_insert, logartist_table_insert, song_select,
+                         song_table_insert, songplay_table_insert, time_table_insert,
+                         user_table_insert)
 
 
 def process_song_file(cur, filepath):
     # open song file
-    df = 
+    df = pd.read_json(filepath, typ='series')
 
     # insert song record
-    song_data = 
+    song_data = df[[
+        'song_id',
+        'title',
+        'artist_id',
+        'year',
+        'duration'
+    ]].values
     cur.execute(song_table_insert, song_data)
-    
+
     # insert artist record
-    artist_data = 
+    artist_data = df[[
+        'artist_id',
+        'artist_name',
+        'artist_location',
+        'artist_latitude',
+        'artist_longitude'
+    ]].values
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df['page'] == 'NextSong']
 
     # convert timestamp column to datetime
-    t = 
-    
+    t = pd.to_datetime(df['ts'])
+    df['ts_timestamp'] = pd.to_datetime(df['ts'])
+
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = (
+        t.values,
+        t.dt.hour.values,
+        t.dt.day.values,
+        t.dt.isocalendar().week.values,
+        t.dt.month.values,
+        t.dt.year.values,
+        t.dt.weekday.values
+    )
+    column_labels = (
+        'start_time',
+        'hour',
+        'day',
+        'week',
+        'month',
+        'year',
+        'weekday'
+    )
+    time_df = pd.DataFrame(data=time_data).T
+    time_df.columns = column_labels
+
+    # Cast datatypes
+    time_df = time_df.astype({
+        'hour': int,
+        'day': int,
+        'week': int,
+        'month': int,
+        'year': int,
+        'weekday': int,
+    })
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[[
+        'userId',
+        'firstName',
+        'lastName',
+        'gender',
+        'level'
+    ]]
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -45,27 +95,46 @@ def process_log_file(cur, filepath):
 
     # insert songplay records
     for index, row in df.iterrows():
-        
+
         # get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
+        # cur.execute(song_select, (row.song, row.artist))
         results = cur.fetchone()
-        
+
         if results:
             songid, artistid = results
         else:
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = (
+            # index,  # TODO replace with auto indexing
+            row['ts_timestamp'],
+            row['userId'],
+            row['level'],
+            songid,
+            artistid,
+            row['sessionId'],
+            row['location'],
+            row['userAgent']
+        )
         cur.execute(songplay_table_insert, songplay_data)
+
+        # Debugging
+        # logartist_data = (row['artist'], row['sessionId'])
+        logartist_data = (row['artist'],)
+        # print(f"row['artist'] = {row['artist']}")
+        # print(f"type(row['artist']) = {type(row['artist'])}")
+        cur.execute(logartist_table_insert, logartist_data)
+        # print(f"cur.query = {cur.query}")
 
 
 def process_data(cur, conn, filepath, func):
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
-        files = glob.glob(os.path.join(root,'*.json'))
-        for f in files :
+        files = glob.glob(os.path.join(root, '*.json'))
+        for f in files:
             all_files.append(os.path.abspath(f))
 
     # get total number of files found
--- sql_queries.py
+++ sql_queries.py
@@ -5,53 +5,54 @@ user_table_drop = "DROP TABLE IF EXISTS users;"
 song_table_drop = "DROP TABLE IF EXISTS songs;"
 artist_table_drop = "DROP TABLE IF EXISTS artists;"
 time_table_drop = "DROP TABLE IF EXISTS time;"
+logartist_table_drop = "DROP TABLE IF EXISTS logartists;"
 
 # CREATE TABLES
 
+        # songplay_id INT,
+        # PRIMARY KEY (songplay_id)
 songplay_table_create = ("""
     CREATE TABLE IF NOT EXISTS songplays (
-        songplay_id int,
         start_time timestamp,
-        user_id int,
-        level varchar,
-        song_id int,
-        artist_id int,
-        session_id int,
-        location varchar,
-        user_agent varchar,
-        PRIMARY KEY (songplay_id)
+        user_id INT,
+        level VARCHAR,
+        song_id VARCHAR,
+        artist_id VARCHAR,
+        session_id INT,
+        location VARCHAR,
+        user_agent VARCHAR
     );
 """)
 
 user_table_create = ("""
     CREATE TABLE IF NOT EXISTS users (
-        user_id int,
-        first_name varchar,
-        last_name varchar,
-        gender varchar,
-        level varchar,
+        user_id INT,
+        first_name VARCHAR,
+        last_name VARCHAR,
+        gender VARCHAR,
+        level VARCHAR,
         PRIMARY KEY (user_id)
     );
 """)
 
 song_table_create = ("""
     CREATE TABLE IF NOT EXISTS songs (
-        song_id varchar,
-        title varchar,
-        artist_id varchar,
-        year int,
-        duration real,
+        song_id VARCHAR,
+        title VARCHAR,
+        artist_id VARCHAR,
+        year INT,
+        duration DECIMAL,
         PRIMARY KEY (song_id)
     );
 """)
 
 artist_table_create = ("""
     CREATE TABLE IF NOT EXISTS artists (
-        artist_id varchar,
-        artist_name varchar,
-        artist_location varchar,
-        artist_latitude real,
-        artist_longitude real,
+        artist_id VARCHAR,
+        artist_name VARCHAR,
+        artist_location VARCHAR,
+        artist_latitude DECIMAL,
+        artist_longitude DECIMAL,
         PRIMARY KEY (artist_id)
     );
 """)
@@ -59,21 +60,29 @@ artist_table_create = ("""
 time_table_create = ("""
     CREATE TABLE IF NOT EXISTS time (
         start_time timestamp,
-        hour int,
-        day int,
-        week int,
-        month int,
-        year int,
-        weekday int,
+        hour INT,
+        day INT,
+        week INT,
+        month INT,
+        year INT,
+        weekday INT,
         PRIMARY KEY (start_time)
+    );
+""")
+
+logartist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS logartists (
+        artist_name VARCHAR,
+        session_id INT,
+        PRIMARY KEY (artist_name)
     );
 """)
 
 # INSERT RECORDS
 
+        # songplay_id,
 songplay_table_insert = ("""
     INSERT INTO songplays (
-        songplay_id,
         start_time,
         user_id,
         level,
@@ -83,7 +92,8 @@ songplay_table_insert = ("""
         location,
         user_agent
     )
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING;
 """)
 
 user_table_insert = ("""
@@ -95,6 +105,8 @@ user_table_insert = ("""
         level
     )
     VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (user_id) DO UPDATE
+    SET first_name = EXCLUDED.first_name;
 """)
 
 song_table_insert = ("""
@@ -117,6 +129,8 @@ artist_table_insert = ("""
         artist_longitude
     )
     VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (artist_id) DO UPDATE
+    SET artist_name = EXCLUDED.artist_name;
 """)
 
 time_table_insert = ("""
@@ -130,14 +144,31 @@ time_table_insert = ("""
         weekday
     )
     VALUES (%s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING;
 """)
+
+# Debugging
+logartist_table_insert = ("""
+    INSERT INTO logartists (
+        artist_name
+    )
+    VALUES (%s)
+    ON CONFLICT (artist_name) DO NOTHING;
+""")
+
 
 # FIND SONGS
 song_select = ("""
-    SELECT song_id, artist_id FROM songs
-    JOIN artists using(artist_id)
-    WHERE title=%s and artist_name=%s and duration=%s
+    SELECT song_id, songs.artist_id FROM songs
+    JOIN artists ON songs.artist_id = artists.artist_id
+    WHERE title=%s AND artist_name=%s AND duration=%s;
 """)
+
+# song_select = ("""
+#     SELECT song_id, songs.artist_id FROM songs
+#     JOIN artists ON songs.artist_id = artists.artist_id
+#     WHERE title=%s AND artist_name=%s;
+# """)
 
 # QUERY LISTS
 
@@ -146,15 +177,14 @@ create_table_queries = [
     user_table_create,
     song_table_create,
     artist_table_create,
-    time_table_create
+    time_table_create,
+    logartist_table_create
 ]
 drop_table_queries = [
     songplay_table_drop,
     user_table_drop,
     song_table_drop,
     artist_table_drop,
-    time_table_drop
+    time_table_drop,
+    logartist_table_drop
 ]
-
-
-# TODO check primary keys
